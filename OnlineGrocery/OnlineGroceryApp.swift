626e6681da52f7c4a17924ef31c714556ba0dcf4
IsuruManawadu97 <manawadugeudara1997@gmail.com>
2025-04-21 07:22:17 +0530

    Initial Commit

---
+++ OnlineGrocery/OnlineGroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceryApp.swift
+//  OnlineGrocery
+//
+//  Created by Isuru Manawadu on 2025-04-21.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnlineGroceryTests/OnlineGroceryTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceryTests.swift
+//  OnlineGroceryTests
+//
+//  Created by Isuru Manawadu on 2025-04-21.
+//
+
+import Testing
+@testable import OnlineGrocery
+
+struct OnlineGroceryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
